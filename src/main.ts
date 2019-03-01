fc5ce742ce32b6d649f446e30e5ad698adc267ec
Ii Masamitsu <arcana278@gmail.com>
2019-03-01 19:21:37 +0900

    警告を取り除いた

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,21 @@
 import path from 'path';
 import { BrowserWindow, app } from 'electron';
 
-let win;
+let win: BrowserWindow | null;
 
 app.on('ready', () => {
   win = new BrowserWindow({
     width: 800,
     height: 600,
+    webPreferences: {
+      nodeIntegration: false,
+    }
   });
   win.on('closed', () => {
     win = null;
   });
   win.loadFile(path.resolve(__dirname, 'index.html'));
+  win.webContents.openDevTools();
 });
 
 app.on('window-all-closed', () => {
